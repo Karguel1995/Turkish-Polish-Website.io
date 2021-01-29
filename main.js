1d76397c61e77a7b402759a9d2876e5a4a9d9322
Filip Pawelec <Karguel1995@gmail.com>
2021-01-29 18:31:02 +0100

    Add "About Us", "Trips", and "Home"

--- main.js
+++ main.js
@@ -27,7 +27,6 @@ const picturesBefore = [...document.querySelectorAll('.image')]
 plFlags.forEach(function (plFlag, index) {
     let polishWords = ['Mleko', 'Kawa', 'Wino', 'Herbata', 'Sok', 'Piwo', 'Woda', 'Miód', 'Zupa', 'Jabłko', 'Banan', 'Pomarańcza', 'Truskawka', 'Winogrono', 'Gruszka', 'Arbuz', 'Cytryna', 'Brzoskwinia', 'Pomidor', 'Ogórek', 'Papryka', 'Cebula', 'Ziemniak', 'Marchewka', 'Burak', 'Kukurydza', 'Sałata']
     const plFlagClick = () => {
-        console.log(`${polishWords[index]} zdjęcie ${picturesBefore[index]}`)
         texts[index].textContent = polishWords[index];
         picturesBefore[index].remove();
         
@@ -46,10 +45,15 @@ trFlags.forEach(function (trFlag, index) {
     let turkishWords = ['Süt', 'Kahve', 'Şarap', 'Çay', 'Meyve suyu', 'Bira', 'Su', 'Bal', 'Çorba', 'Elma', 'Muz', 'Portakal', 'Çilek', 'Üzüm', 'Armut', 'Karpuz', 'Limon', 'Şeftali', 'Domates', 'Salatalık', 'Biber', 'Soğan', 'Patates', 'Havuç', 'Pancar kökü', 'Mısır', 'Marul']
 
     const trFlagClick = () => {
-        console.log(turkishWords[index])
         texts[index].textContent = turkishWords[index];
         picturesBefore[index].remove();
-        image.remove();
+        // image.remove();
+        texts.forEach(function(text, index) { 
+        text.addEventListener('click', function() {
+            sectionPicture[index].append(picturesBefore[index])
+            // sectionPicture[index].append(img.src = `TRPLPic/${pictures[index]}.jpg`)
+        })
+    })
     }
 
     trFlag.addEventListener('click', trFlagClick)
